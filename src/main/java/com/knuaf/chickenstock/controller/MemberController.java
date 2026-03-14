39411063d8a48ab39e7d252fa27911f45485228b
jangsion23 <sionjang23@naver.com>
2026-03-14 16:51:00 +0900

    로그인 및 세션 값 출력

--- src/main/java/com/knuaf/chickenstock/controller/MemberController.java
+++ src/main/java/com/knuaf/chickenstock/controller/MemberController.java
@@ -1,4 +1,48 @@
 package com.knuaf.chickenstock.controller;
 
+
+import com.knuaf.chickenstock.dto.MemberDTO;
+import com.knuaf.chickenstock.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDto) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDto);
+
+        memberService.save(memberDto);
+        return "index";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login") // Session : 로그인 유지
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        } else {
+            // login 실패
+            return "login";
+        }
+    }
 }
--- src/main/java/com/knuaf/chickenstock/repository/MemberRepository.java
+++ src/main/java/com/knuaf/chickenstock/repository/MemberRepository.java
@@ -1,4 +1,14 @@
 package com.knuaf.chickenstock.repository;
 
-public interface MemberRepository {
+import com.knuaf.chickenstock.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    Optional<MemberEntity> findByMemberEmail(String memberEmail);
 }
